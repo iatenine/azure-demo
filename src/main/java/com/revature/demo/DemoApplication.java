4abef69f9724d19b8e1e45c60e4346c55babb65e
Jack <iatenine@gmail.com>
2022-03-14 10:38:12 -0600

    Correct EntityScan with ComponentScan

--- src/main/java/com/revature/demo/DemoApplication.java
+++ src/main/java/com/revature/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.revature.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.revature")
 public class DemoApplication {
 
 	public static void main(String[] args) {
